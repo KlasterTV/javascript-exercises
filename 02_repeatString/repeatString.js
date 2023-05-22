d73dd495b4fa6302a97f5a25f24e5c0d070e849d
KlasterTV <elasticgaming10@gmail.com>
2023-05-21 22:05:33 -0400

    Finished tasks up to removearray

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  let repeatedString = "";
+  for (let i = 0; i < times; i++) {
+    repeatedString += str;
+  }
+  return repeatedString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let splitstring = str.split("");
+  let reverseArray = splitstring.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
+
+// return str.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
