42b750b7859994c7bcb56bcade6a5c70e567bb29
KlasterTV <elasticgaming10@gmail.com>
2023-05-23 19:57:32 -0400

    Finished sumAll and leapYears, Tempconversion next

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (
+    typeof start !== "number" ||
+    typeof end !== "number" ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
+  const arr = [];
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  let sum = 0;
+  arr.forEach((each) => {
+    sum += each;
+  });
+  return sum;
 };
 
 // Do not edit below this line
